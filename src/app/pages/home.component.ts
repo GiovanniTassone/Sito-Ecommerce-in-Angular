076e3ebc5d8419948588e0c631c0b3c2224f7a73
GiovanniTassone <tassonegiovanni33@gmail.com>
2022-05-13 15:35:17 +0200

    Miogliorie estetiche della pagina dei prodotti

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -10,18 +10,23 @@ import { ProductService } from '../service/product.service';
       class="row row-cols-1 row-cols-md-3 g-1 d-flex justify-content-center gap-4 mt-3 w-75 mx-auto"
     >
       <div
-        class="card d-flex align-items-baseline"
+        class="card d-flex align-items-between"
         style="width: 18rem;height: 35rem"
         *ngFor="let product of productsList"
       >
-        <img src="{{ product.url }}" class="card-img-top mt-3" alt="..." />
-        <div class="card-body">
+        <img
+          src="{{ product.url }}"
+          class="card-img-top mt-3 mx-auto"
+          alt="..."
+          style="width: 15rem;height: 12rem"
+        />
+        <div class="card-body d-flex align-content-between flex-column ">
           <h5 class="card-title">{{ product.name | uppercase }}</h5>
           <p class="card-text">{{ product.price | currency: 'EUR' }}</p>
           <p class="card-text">{{ product.description }}</p>
           <button
             [routerLink]="['/product', product.id]"
-            class="btn btn-primary"
+            class="btn btn-primary mt-auto"
           >
             Dettagli
           </button>
